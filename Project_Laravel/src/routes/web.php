6283c0d4bdf2c4293b6a96d0e9b85e9b299ddc3d
Truong Minh Thang <truongminhthang10@gmail.com>
2025-12-17 03:07:39 +0700

    Add migration route

--- Project_Laravel/src/routes/web.php
+++ Project_Laravel/src/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-migrate', function () {
+    Artisan::call('migrate --force');
+    return 'Đã tạo bảng Database thành công!';
+});
